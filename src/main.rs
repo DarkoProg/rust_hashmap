30fd8230f854d9ef97ce26332b49f44b6e8057e1
DarkoProg <marko.golob99@gmail.com>
2024-06-18 00:17:31 +0200

    added tasks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Make a big array
+// Get a magic function that converts elements into integers (hashing)
+// Store elements in the array at the index specified by their magic integer
+// Do something interesting if two elements get the same index (hash conflict) (robinhood hashmap)
 fn main() {
     println!("Hello, world!");
 }
